cdf3c74bd302cc44c20c7dcc723265e05e61db62
NaliniVinayagam13 <112850747+NaliniVinayagam13@users.noreply.github.com>
2025-09-16 06:29:41 +0000

    Regenerate android and ios with expo prebuild

--- RNAudioTaskApp/android/app/src/main/java/com/rnaudiotaskapp/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.rnaudiotaskapp
-
-import com.facebook.react.ReactActivity
-
-class MainActivity: ReactActivity() {
-  override fun getMainComponentName(): String? {
-    return "RNAudioTaskApp"
-  }
-}
